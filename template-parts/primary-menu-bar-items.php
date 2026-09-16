43faba359de6e6ffbb3d915fd9e3dacb41ed21c9
Kel Weaver <kel.weaver@gmail.com>
2026-09-16 04:21:55 +0000

    Add dropdown support and pipe separators to Night's nav bar
    Primary menu items with children now render as a button + hover/focus
    dropdown, same markup and behavior as the existing Latest item, so a
    submenu (e.g. CV > Home) works identically. Items with no children
    stay plain links. Added a "|" separator between each top-level item
    in the horizontal bar with balanced spacing on both sides.

--- template-parts/primary-menu-bar-items.php
+++ template-parts/primary-menu-bar-items.php
@@ -2,21 +2,51 @@
 $kelweaver_nav_locations = get_nav_menu_locations();
 
 if ( isset( $kelweaver_nav_locations['primary'] ) ) {
-	$kelweaver_primary_items = wp_get_nav_menu_items( $kelweaver_nav_locations['primary'] );
+	$kelweaver_menu_items = wp_get_nav_menu_items( $kelweaver_nav_locations['primary'] );
 
-	if ( $kelweaver_primary_items ) {
-		foreach ( $kelweaver_primary_items as $kelweaver_menu_item ) {
+	if ( $kelweaver_menu_items ) {
+		$kelweaver_children = array();
 
-			if ( $kelweaver_menu_item->menu_item_parent ) {
+		foreach ( $kelweaver_menu_items as $kelweaver_item ) {
+			if ( $kelweaver_item->menu_item_parent ) {
+				$kelweaver_children[ $kelweaver_item->menu_item_parent ][] = $kelweaver_item;
+			}
+		}
+
+		foreach ( $kelweaver_menu_items as $kelweaver_item ) {
+
+			if ( $kelweaver_item->menu_item_parent ) {
 				continue;
 			}
-			?>
-			<li class="site-nav-bar-item">
-				<a class="site-nav-bar-link" href="<?php echo esc_url( $kelweaver_menu_item->url ); ?>">
-					<?php echo esc_html( $kelweaver_menu_item->title ); ?>
-				</a>
-			</li>
-			<?php
+
+			$kelweaver_item_children = isset( $kelweaver_children[ $kelweaver_item->ID ] ) ? $kelweaver_children[ $kelweaver_item->ID ] : array();
+
+			if ( $kelweaver_item_children ) :
+				?>
+				<li class="site-nav-bar-item">
+					<button type="button" class="site-nav-bar-link" aria-haspopup="true" aria-expanded="false">
+						<?php echo esc_html( $kelweaver_item->title ); ?>
+					</button>
+					<ul class="site-nav-bar-dropdown">
+						<?php foreach ( $kelweaver_item_children as $kelweaver_child ) : ?>
+							<li>
+								<a href="<?php echo esc_url( $kelweaver_child->url ); ?>">
+									<?php echo esc_html( $kelweaver_child->title ); ?>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</li>
+				<?php
+			else :
+				?>
+				<li class="site-nav-bar-item">
+					<a class="site-nav-bar-link" href="<?php echo esc_url( $kelweaver_item->url ); ?>">
+						<?php echo esc_html( $kelweaver_item->title ); ?>
+					</a>
+				</li>
+				<?php
+			endif;
 		}
 	}
 }
